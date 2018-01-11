f8d78c3360c9a68a64a4185d3bc47d5153d6e1da
robin521111 <Robin,Lu@sonymobile.com>
2018-01-11 16:13:25 +0800

    commit for 1.6 courses homework

--- 1.6 homework/knn_iris.py
+++ 1.6 homework/knn_iris.py
@@ -1,5 +1,7 @@
 import numpy as np
 from sklearn import datasets
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.cross_validation import train_test_split
 
 iris = datasets.load_iris()
 
@@ -9,20 +11,37 @@ Y=iris.target
 print(iris.target)
 print(iris.data)
 
-from sklearn.cross_validation import train_test_split
 
 X_train, X_test, Y_train, Y_test=train_test_split(X,Y,test_size=.75)
 
-from sklearn.neighbors import KNeighborsClassifier
 
-classifier = KNeighborsClassifier()
+print(X_train)
+print('=========')
+print(X_test)
+print('=========')
+
+print(Y_train)
+print('=========')
+
+print(Y_test)
+
+
+classifier = KNeighborsClassifier(n_neighbors=5)
 
 classifier.fit(X_train,Y_train)
 
-predictions=classifier.predict(X_test)
+Y_pred=classifier.predict(X_test)
 
 from sklearn.metrics import accuracy_score
-print(accuracy_score(X_test,predictions))
+acc = accuracy_score(Y_pred, Y_test)
+accPer = '{:.1%}'.format(acc)
 
 
+sepalLength = input("Enter a Sepal Length: ")
+sepalWidth = input("Enter a Sepal Width: ")
+petalLength = input("Enter a Petal Length: ")
+petalWidth = input("Enter a Petal Width: ")
 
+guess = iris.target_names[classifier.predict([[sepalLength, sepalWidth, petalLength, petalWidth]])]
+
+print("The probably species is %s. With a test accuracy of %s"%(guess,accPer))
