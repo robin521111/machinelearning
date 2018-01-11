6dad3ab746274ea32b9fb83efb169a3206b76128
robin521111 <Robin,Lu@sonymobile.com>
2018-01-11 14:08:26 +0800

    commit for home work 1.6 1.7

---
+++ 1.6 homework/knn_iris.py
@@ -0,0 +1,28 @@
+import numpy as np
+from sklearn import datasets
+
+iris = datasets.load_iris()
+
+X=iris.data
+Y=iris.target
+
+print(iris.target)
+print(iris.data)
+
+from sklearn.cross_validation import train_test_split
+
+X_train, X_test, Y_train, Y_test=train_test_split(X,Y,test_size=.75)
+
+from sklearn.neighbors import KNeighborsClassifier
+
+classifier = KNeighborsClassifier()
+
+classifier.fit(X_train,Y_train)
+
+predictions=classifier.predict(X_test)
+
+from sklearn.metrics import accuracy_score
+print(accuracy_score(X_test,predictions))
+
+
+
