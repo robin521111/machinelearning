7352fb5e37ac428834d5b740117d3793b40d2da1
Robin, Lu <luhaibin731@gmail.com>
2018-01-19 19:44:27 +0800

    upload feature_homework of 1.13

---
+++ 1.13_homework/feature_engineering_2.2.py
@@ -0,0 +1,10 @@
+from sklearn import preprocessing
+from sklearn.datasets import load_iris
+from sklearn.feature_selection import RFE
+from sklearn.linear_model import LogisticRegression
+iris = load_iris()
+
+selector = RFE(estimator=LogisticRegression(),n_features_to_select=4).fit(iris.data,iris.target)
+data = selector.transform(iris.data)
+print(data)
+print(selector.ranking_)
